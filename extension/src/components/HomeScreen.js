b3009a571b1e79c593a709e7265c06867bc92e69
SphinxNumberNine <ashwathkrishnan@yahoo.com>
2019-07-14 20:39:23 -0400

    fixed manifest

--- extension/src/components/HomeScreen.js
+++ extension/src/components/HomeScreen.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { Container, Grid, Typography, Button } from '@material-ui/core';
-import ErrorLogo from '@material-ui/icons/ErrorOutline';
-import PhoneLockLogo from '@material-ui/icons/ScreenLockPortrait';
-import CloudLogo from '@material-ui/icons/CloudOutlined';
+import React, { Component } from "react";
+import { Container, Grid, Typography, Button } from "@material-ui/core";
+import ErrorLogo from "@material-ui/icons/ErrorOutline";
+import PhoneLockLogo from "@material-ui/icons/ScreenLockPortrait";
+import CloudLogo from "@material-ui/icons/CloudOutlined";
 
-import RalewayExtraBold from '../assets/fonts/Raleway-ExtraBold.ttf';
+import RalewayExtraBold from "../assets/fonts/Raleway-ExtraBold.ttf";
 
 class HomeScreen extends Component {
   constructor(props) {
@@ -14,19 +14,19 @@ class HomeScreen extends Component {
 
   render() {
     return (
-      <Container style={{ textAlign: 'left' }}>
-        <Grid container alignItems='center' direction='row' noWrap>
+      <Container style={{ textAlign: "left" }}>
+        <Grid container alignItems="center" direction="row" noWrap>
           <Grid item>
             <Typography
               style={{
-                fontSize: '45px',
-                marginTop: '40px',
-                marginLeft: '0px',
-                color: '#854DFF',
-                fontWeight: 'bold'
+                fontSize: "45px",
+                marginTop: "40px",
+                marginLeft: "0px",
+                color: "#854DFF",
+                fontWeight: "bold"
               }}
-              variant='h4'
-              component='h4'
+              variant="h4"
+              component="h4"
               gutterBottom
               noWrap
             >
@@ -34,13 +34,13 @@ class HomeScreen extends Component {
             </Typography>
             <Typography
               style={{
-                fontSize: '45px',
-                marginLeft: '0px',
-                color: '#854DFF',
-                fontWeight: 'bold'
+                fontSize: "45px",
+                marginLeft: "0px",
+                color: "#854DFF",
+                fontWeight: "bold"
               }}
-              variant='h4'
-              component='h4'
+              variant="h4"
+              component="h4"
               gutterBottom
               noWrap
             >
@@ -48,20 +48,20 @@ class HomeScreen extends Component {
             </Typography>
             <Typography
               style={{
-                fontSize: '25px',
-                marginTop: '50px',
-                marginLeft: '0px '
+                fontSize: "25px",
+                marginTop: "50px",
+                marginLeft: "0px "
               }}
-              variant='h6'
-              component='h6'
+              variant="h6"
+              component="h6"
               noWrap
             >
               Use Blacklight's AI to
             </Typography>
             <Typography
-              style={{ fontSize: '25px', marginLeft: '0px ' }}
-              variant='h6'
-              component='h6'
+              style={{ fontSize: "25px", marginLeft: "0px " }}
+              variant="h6"
+              component="h6"
               gutterBottom
               noWrap
             >
@@ -69,92 +69,92 @@ class HomeScreen extends Component {
             </Typography>
             <Grid
               container
-              direction='row'
-              alignItems='center'
-              style={{ marginBottom: '7px' }}
+              direction="row"
+              alignItems="center"
+              style={{ marginBottom: "7px" }}
               noWrap
             >
               <Grid item noWrap>
                 <ErrorLogo
                   style={{
-                    marginLeft: '25px',
-                    marginRight: '10px',
-                    color: '#854DFF',
-                    verticalAlign: 'middle'
+                    marginLeft: "25px",
+                    marginRight: "10px",
+                    color: "#854DFF",
+                    verticalAlign: "middle"
                   }}
                 />
               </Grid>
               <Grid item noWrap>
-                <Typography style={{ verticalAlign: 'middle' }}>
+                <Typography style={{ verticalAlign: "middle" }}>
                   Unwanted Data Sale
                 </Typography>
               </Grid>
             </Grid>
             <Grid
               container
-              direction='row'
-              alignItems='center'
-              style={{ marginBottom: '7px' }}
+              direction="row"
+              alignItems="center"
+              style={{ marginBottom: "7px" }}
               noWrap
             >
               <Grid item noWrap>
                 <ErrorLogo
                   style={{
-                    marginLeft: '25px',
-                    marginRight: '10px',
-                    color: '#854DFF',
-                    verticalAlign: 'middle'
+                    marginLeft: "25px",
+                    marginRight: "10px",
+                    color: "#854DFF",
+                    verticalAlign: "middle"
                   }}
                 />
               </Grid>
               <Grid item noWrap>
-                <Typography style={{ verticalAlign: 'middle' }}>
+                <Typography style={{ verticalAlign: "middle" }}>
                   Unwanted Location Sharing
                 </Typography>
               </Grid>
             </Grid>
             <Grid
               container
-              direction='row'
-              alignItems='center'
-              style={{ marginBottom: '7px' }}
+              direction="row"
+              alignItems="center"
+              style={{ marginBottom: "7px" }}
               noWrap
             >
               <Grid item noWrap>
                 <ErrorLogo
                   style={{
-                    marginLeft: '25px',
-                    marginRight: '10px',
-                    color: '#854DFF',
-                    verticalAlign: 'middle'
+                    marginLeft: "25px",
+                    marginRight: "10px",
+                    color: "#854DFF",
+                    verticalAlign: "middle"
                   }}
                 />
               </Grid>
               <Grid item noWrap>
-                <Typography style={{ verticalAlign: 'middle' }}>
+                <Typography style={{ verticalAlign: "middle" }}>
                   Misleading Language
                 </Typography>
               </Grid>
             </Grid>
             <Grid
               container
-              direction='row'
-              alignItems='center'
-              style={{ marginBottom: '7px' }}
+              direction="row"
+              alignItems="center"
+              style={{ marginBottom: "7px" }}
               noWrap
             >
               <Grid item noWrap>
                 <ErrorLogo
                   style={{
-                    marginLeft: '25px',
-                    marginRight: '10px',
-                    color: '#854DFF',
-                    verticalAlign: 'middle'
+                    marginLeft: "25px",
+                    marginRight: "10px",
+                    color: "#854DFF",
+                    verticalAlign: "middle"
                   }}
                 />
               </Grid>
               <Grid item noWrap>
-                <Typography style={{ verticalAlign: 'middle' }}>
+                <Typography style={{ verticalAlign: "middle" }}>
                   Much, Much More
                 </Typography>
               </Grid>
@@ -176,31 +176,31 @@ class HomeScreen extends Component {
     }
 
     return (
-      <Grid item justify='center' noWrap>
+      <Grid item justify="center" noWrap>
         <PhoneLockLogo
           style={{
-            height: '300px',
-            width: '300px',
-            marginLeft: '30px',
-            marginTop: '30px',
-            color: '#606060'
+            height: "300px",
+            width: "300px",
+            marginLeft: "30px",
+            marginTop: "30px",
+            color: "#606060"
           }}
         />
-        <Typography style={{ marginLeft: '100px', fontSize: '20px' }} noWrap>
+        <Typography style={{ marginLeft: "100px", fontSize: "20px" }} noWrap>
           Keep Us Humble.
         </Typography>
         <Button
-          variant='outlined'
-          label='Scan Our Policy'
-          size='large'
+          variant="outlined"
+          label="Scan Our Policy"
+          size="large"
           style={{
-            marginTop: '10px',
-            marginLeft: '87px',
+            marginTop: "10px",
+            marginLeft: "87px",
             borderRadius: 25,
-            fontWeight: 'bolder',
-            fontSize: '15px',
-            backgroundColor: '#854DFF',
-            color: '#DBDBDB'
+            fontWeight: "bolder",
+            fontSize: "15px",
+            backgroundColor: "#854DFF",
+            color: "#DBDBDB"
           }}
         >
           SCAN OUR POLICY
