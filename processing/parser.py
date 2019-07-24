3df9c098f083ceabebc6e243da938d1c13bc535c
prithviokade <okadeprithvi@gmail.com>
2019-07-24 16:09:24 -0400

    completed phraser with document reader

--- processing/parser.py
+++ processing/parser.py
@@ -2,12 +2,8 @@ from stat_parser import Parser
 import re
 import string
 
-parser = Parser()
-
 ## make sure this is in pyStatParser folder
-
-#--> needs to be tested
-
+parser = Parser()
 
 
 # https://stackoverflow.com/questions/5454322/python-how-to-match-nested-parentheses-with-regex
@@ -63,7 +59,7 @@ def create_stack(tree):
 
     return stack
 
-# converts stack (list) to string
+# converts stack contents to string
 def stack_to_string(stack):
     return " ".join(stack).strip()
 
@@ -72,10 +68,12 @@ def strip_space(sent):
             
 def run(sentence):
     if sentence[-1] in string.punctuation:
-        sentence = sentence[:-1]
-
-    print(parser.parse(sentence))
-    print("\n")
+        sentence = sentence[:-1]   #remove ending punctuation
+        if sentence == "":
+            return
+    
+    #print(parser.parse(sentence))
+    #print("\n")
 
     stack = create_stack(parser.parse(sentence)) 
     sent = stack_to_string(stack)
@@ -94,7 +92,78 @@ def run(sentence):
         if phrase not in phrase_list:
             phrase_list.append(phrase)
 
-    print(phrase_list)
+    #print(phrase_list)
     return phrase_list
     
+'''
 run("We receive and store any information.")
+run("We don't recieve and store any information") 
+    # --> include that "n't" and "not" are identical
+run("We do not recieve and store any information")
+'''
+
+#112
+def readFile(path):
+    with open(path, "rt") as f:
+        return f.read()
+
+#112
+def writeFile(path, contents):
+    with open(path, "wt") as f:
+        f.write(contents)
+
+#https://stackoverflow.com/questions/4576077/python-split-text-on-sentences
+#splits document into sentences
+alphabets= "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov)"
+
+def split_into_sentences(text):
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
+
+#reads file and parses all sentences
+def parse_all(path):
+    tos = readFile(path)
+    
+    #edit file to make it readable for parser
+    #this needs some customization depending on the text format
+    tos = tos.replace("\n",". ")
+    tos = tos.replace("(", "[")
+    tos = tos.replace(")", "]")
+    tos = tos.replace(":\n", ".")
+    tos = tos.replace(":",",")
+    
+    phrase_list = []
+    for sentence in split_into_sentences(tos):
+        print(sentence)
+        phrase_list.append(run(sentence))
+    
+    return set(phrase_list)
+
+print(parse_all("facebook_tos.txt"))
